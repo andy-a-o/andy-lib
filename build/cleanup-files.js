e51b9363388d1e973e6d40cd622a0440dfb86745
Andrew Anisimov <anisimov.new@gmail.com>
2017-07-20 18:01:39 +0300

    Chane ES Module file suffix from .esm.ts to .es.ts

--- build/cleanup-files.js
+++ build/cleanup-files.js
@@ -1,4 +1,4 @@
 const del = require('del');
-del(['dist/src/!(*.d.ts)', 'dist/!(*.umd.js|*.esm.js|*.d.ts|*.umd.js.map|*.esm.js.map|src)']).then(paths => {
+del(['dist/src/!(*.d.ts)', 'dist/!(*.umd.js|*.es.js|*.d.ts|*.umd.js.map|*.es.js.map|src)']).then(paths => {
     console.log('Files and folders that would be deleted:\n', paths.join('\n'));
 });
