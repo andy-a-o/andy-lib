54745b497a5c71c3d2b7958e3c6a2bddfc761d70
Andrew Anisimov <anisimov.new@gmail.com>
2017-07-20 18:04:50 +0300

    use build scripts from @andy-lib/build

--- util/config-library.js
+++
@@ -1,3 +0,0 @@
-export const nameLibrary = "util";
-export const PATH_SRC = "lib/";
-export const PATH_DIST = "dist/";
--- util/tools/removecode.js
+++
@@ -1,4 +0,0 @@
-const del = require('del');
-del(['dist/src/!(*.d.ts)', 'dist/!(*.umd.js|*.esm.js|*.d.ts|*.umd.js.map|*.esm.js.map|src)']).then(paths => {
-    console.log('Files and folders that would be deleted:\n', paths.join('\n'));
-});
