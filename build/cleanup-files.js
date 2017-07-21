81d2ff645c70858ff0aa53680a0242f8ca871818
Andrew Anisimov <anisimov.new@gmail.com>
2017-07-21 10:20:49 +0300

    Fixed issues with improperly deleted package files in @andy-lib/build

--- build/cleanup-files.js
+++ build/cleanup-files.js
@@ -1,4 +1,4 @@
 const del = require('del');
-del(['dist/src/!(*.d.ts)', 'dist/!(*.umd.js|*.es.js|*.d.ts|*.umd.js.map|*.es.js.map|src)']).then(paths => {
+del(['dist/src/**/*.js', 'dist/src/**/*.js.map', 'dist/!(*.umd.js|*.es.js|*.d.ts|*.umd.js.map|*.es.js.map|src)']).then(paths => {
     console.log('Files and folders that would be deleted:\n', paths.join('\n'));
 });
